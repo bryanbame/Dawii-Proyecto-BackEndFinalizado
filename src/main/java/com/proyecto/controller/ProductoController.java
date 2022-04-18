67f0d75dbd3ab9db6604bf40aaae0df880e817c7
ace777j <axe_tazlok126@hotmail.com>
2022-04-18 00:10:25 -0500

    Producto

--- src/main/java/com/proyecto/controller/ProductoController.java
+++ src/main/java/com/proyecto/controller/ProductoController.java
@@ -1,9 +1,21 @@
 package com.proyecto.controller;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.proyecto.entidad.Producto;
+import com.proyecto.service.ProductoService;
 import com.proyecto.util.AppSettings;
 
 @RestController
@@ -11,4 +23,35 @@ import com.proyecto.util.AppSettings;
 @CrossOrigin(origins = AppSettings.URL_CROSS_ORIGIN)
 public class ProductoController {
 
+	@Autowired
+	private ProductoService service;
+
+	@GetMapping
+	@ResponseBody
+	public ResponseEntity<List<Producto>> listaProducto() {
+		List<Producto> lista = service.listaProducto();
+		return ResponseEntity.ok(lista);
+	}
+	
+	@PostMapping
+	@ResponseBody
+	public ResponseEntity<HashMap<String, Object>> insertaProducto(@RequestBody Producto obj) {
+		HashMap<String, Object> salida = new HashMap<String, Object>();
+		try {
+			
+				obj.setIdProducto(0);// Para que registre, sino actualiza
+				Producto objSalida = service.insertaProducto(obj);
+				if (objSalida == null) {
+					salida.put("mensaje", "Error en el registro");
+				} else {
+					salida.put("mensaje", "Se registro correctamente");
+				}
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", "Error en el registro");
+		}
+		return ResponseEntity.ok(salida);
+	}
 }
