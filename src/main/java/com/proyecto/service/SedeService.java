12467bb8318469537c5542b4f344b268f192d292
JimmyBarzola <jbarzolag7@gmail.com>
2022-05-17 06:03:53 -0500

    Consulta Sede

--- src/main/java/com/proyecto/service/SedeService.java
+++ src/main/java/com/proyecto/service/SedeService.java
@@ -8,5 +8,6 @@ public interface SedeService {
 
 	public abstract List<Sede> listaSede();
 	public abstract Sede insertaSede(Sede obj);
+	public abstract List<Sede> listaSedePorNomDirEstPais(String nombre, String direccion, int estado,String codigoPostal, int idPais);
 	
 }
