ccabdd757cef4fac7c37270e781b053d3a510d34
bryanbame <bryan_pkm@hotmail.com>
2022-05-31 13:12:39 -0500

    Crud

--- src/main/java/com/proyecto/repository/MarcaRepository.java
+++ src/main/java/com/proyecto/repository/MarcaRepository.java
@@ -11,4 +11,7 @@ public interface MarcaRepository extends JpaRepository<Marca, Integer>{
 	
 	@Query("select m from Marca m where ( ?1 is '' or m.nombre like ?1 ) and ( ?2 is '' or m.certificado = ?2 ) and ( ?3 is -1  or m.pais.idPais = ?3 ) and (m.estado = ?4) and (?5 is '' or ?6 is '' or (m.fechaRegistro between ?5  and ?6 ))")
 	public abstract List<Marca> listaMarcaPorNombreDescCertPais(String nombre, String certificado, int idPais, int estado, String fecInicio, String fecFin);
+	
+	@Query("select m from Marca m where m.nombre like ?1")
+	public List<Marca> listaPorNombreLike(String nombre);
 }
