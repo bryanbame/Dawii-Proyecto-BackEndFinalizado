393ec521e2d2df1fc090f68bcd32b725a1b6d963
ace777j <axe_tazlok126@hotmail.com>
2022-06-19 18:33:42 -0500

    Mantenimiento Producto

--- src/main/java/com/proyecto/repository/ProductoRepository.java
+++ src/main/java/com/proyecto/repository/ProductoRepository.java
@@ -11,4 +11,6 @@ public interface ProductoRepository extends JpaRepository<Producto, Integer>{
 	@Query("select p from Producto p where ( ?1 is '' or p.nombre like ?1 ) and ( ?2 is '' or p.serie = ?2 ) and ( ?3 is -1 or p.marca.idMarca = ?3 ) and ( ?4 is -1 or p.pais.idPais = ?4 ) and p.estado = ?5")
 	public abstract List<Producto> listaProductosPorFiltro(String nombre, String serie, int idMarca, int idPais, int estado);
 	
+	@Query("select p from Producto p where p.nombre like ?1")
+	public List<Producto> listaPorNombreLike(String nombre);
 }
--- src/main/java/com/proyecto/service/ProductoServiceImpl.java
+++ src/main/java/com/proyecto/service/ProductoServiceImpl.java
@@ -1,7 +1,7 @@
 package com.proyecto.service;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,5 +28,21 @@ public class ProductoServiceImpl implements ProductoService {
 	public List<Producto> listaProductosPorFiltro(String nombre, String serie, int idMarca, int idPais, int estado) {
 		
 		return repository.listaProductosPorFiltro(nombre, serie, idMarca, idPais, estado);
+	}
+
+	@Override
+	public List<Producto> listaProductoPorNombreLike(String nombre) {
+		return repository.listaPorNombreLike(nombre);
+	}
+
+	@Override
+	public void eliminaProducto(int id) {
+		repository.deleteById(id); 
+		
+	}
+
+	@Override
+	public Optional<Producto> buscaProducto(int id) {
+		return repository.findById(id);
 	}
 }
